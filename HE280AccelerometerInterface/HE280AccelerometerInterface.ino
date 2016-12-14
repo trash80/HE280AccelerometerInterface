92c3af675ef4005aabb28a5024bcea05bc1ea752
timothy lamb <trash80@gmail.com>
2016-12-13 16:12:52 -0800

    Updating pinout configuration to use Duet Z probe.

--- HE280AccelerometerInterface/HE280AccelerometerInterface.ino
+++ HE280AccelerometerInterface/HE280AccelerometerInterface.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
 
 uint8_t click_pin = 2; // "INT" interrupt pin from HE280 - Blue on my model
-uint8_t reset_pin = 3; // Used to re-send the initialization code to the HE280's accelerometer. I used the Z-Access homing switch, input side, not ground.
-uint8_t trigger_pin = 13; // Sends a HIGH signal when probe touches the table. Connected to Duet's E0 Stop input. Duet set to Type: 4.
+uint8_t reset_pin = 3; // Used to re-send the initialization code to the HE280's accelerometer. Z Probe Mod pin.
+uint8_t trigger_pin = 13; // Sends a HIGH signal when probe touches the table. Connected to Duet's Z probe in. Duet set to Type: 5.
 
 boolean resetSwitch = false;
 
@@ -18,16 +18,18 @@ void loop() {
     digitalWrite(trigger_pin,HIGH);
     delay(100);
     digitalWrite(trigger_pin,LOW);
-    accelerometer_read(0x31);
+    Serial.print("Accel hit: ");
+    Serial.println(accelerometer_read(0x31));
   }
   if(!digitalRead(reset_pin)) {
-    if(!resetSwitch) {
       resetSwitch = true;
+  } else {
+    if(resetSwitch == true) {
+      Serial.println("Reset Accel");
       accelerometer_read(0x31);
       accelerometer_init();
       delay(1500);
     }
-  } else {
     resetSwitch = false;
   }
 }
